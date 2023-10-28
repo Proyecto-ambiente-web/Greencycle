b439c6c6f8e7782a2121a59b60a19f802d239d9b
IsmaQuesada <quesadasalaci@gmail.com>
2023-10-27 18:28:36 -0600

    Prueba 2.0

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,7 +17,6 @@ import { useEffect } from "react";
 import UsuarioService from "../../services/UsuarioService";
 import InfoIcon from '@mui/icons-material/Info';
 
-// Probando
 function Header() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
