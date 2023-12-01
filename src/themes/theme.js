4a85e0cc9ffab0a90083b9a0d901d648d14a3e8d
Felipe <luisfelipetrejos28@gmail.com>
2023-11-30 21:53:05 -0600

    se agrego un nuevo tema

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,27 +1,30 @@
 import { createTheme } from '@mui/material/styles';
+//101820
+//008000
+
 export const appTheme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-          main: '#215b4a',
-          contrastText: '#f0f9f5',
-        },
-        secondary: {
-          main: '#3b8e74',
-        },
-        background: {
-          default: '#dbf0e5',
-          paper: '#f0f9f5',
-        },
-        text: {
-          primary: '#0d211c',
-          secondary: '#0d211c',
-          disabled: '#0d211c',
-          hint: '#0d211c',
-        },
-        warning: {
-          main: '#ed6c02',
-        },
-        divider: '#bae0ce',
-      },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#101820',
+      contrastText: '#f0f9f5',
+    },
+    secondary: {
+      main: '#008000',
+    },
+    background: {
+      default: '#ffffff',
+      paper: '#ffffff',
+    },
+    text: {
+      primary: '#0d211c',
+      secondary: '#0d211c',
+      disabled: '#0d211c',
+      hint: '#0d211c',
+    },
+    warning: {
+      main: '#ed6c02',
+    },
+    divider: '#bae0ce',
+  },
 }); 
